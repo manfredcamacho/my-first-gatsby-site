52b75eeaf7ad93e1744690586aade4d4e64ba843
Manfred Camacho <manfredcb@gmail.com>
2023-04-30 00:46:02 -0400

    finish part 5

--- gatsby-config.js
+++ gatsby-config.js
@@ -10,6 +10,7 @@ module.exports = {
   plugins: [
     "gatsby-plugin-image",
     "gatsby-plugin-sharp",
+    "gatsby-plugin-mdx",
     {
       resolve: "gatsby-source-filesystem",
       options: {
