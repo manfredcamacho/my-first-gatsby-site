a37313d65c83b92603b03d0a46fabd81c3b14d52
Manfred Camacho <manfredcb@gmail.com>
2023-04-30 01:13:44 -0400

    finish part 6

--- src/pages/blog/index.js
+++ src/pages/blog/index.js
@@ -1,14 +1,19 @@
 import * as React from "react";
-import Layout from "../components/Layout";
-import Seo from "../components/Seo";
+import Layout from "../../components/Layout";
+import Seo from "../../components/Seo";
 import { graphql } from "gatsby";
+import { Link } from "gatsby";
 
 const BlogPage = ({ data }) => {
   return (
     <Layout pageTitle="My Blog Posts">
       {data.allMdx.nodes.map((node) => (
         <article key={node.id}>
-          <h2>{node.frontmatter.title}</h2>
+          <h2>
+            <Link to={`/blog/${node.frontmatter.slug}`}>
+              {node.frontmatter.title}
+            </Link>
+          </h2>
           <p>Posted: {node.frontmatter.date}</p>
           <p>{node.excerpt}</p>
         </article>
